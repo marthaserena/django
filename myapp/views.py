7397b1475189ceae8850eea0bff0f7a525d3dd24
Ronald Wamimbi <trevorwamy@gmail.com>
2022-06-21 12:52:09 +0300

    updated files

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "myapp/userpage.html")
+    
 
 def login(request):
     return render(request, "myapp/login.html")
