b81dfd4cc573ce0ec873573ed3baee67a913b323
Ronald Wamimbi <trevorwamy@gmail.com>
2022-06-21 14:34:09 +0300

    updated login details

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,3 @@
-from django import views
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -7,4 +6,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('login/', views.login, name="login"),
     path('register/', views.register, name="register")
+    
 ]
