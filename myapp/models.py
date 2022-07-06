3e2f986aa7cdd88932329fb6c8e713571d4847f7
Ronald Wamimbi <trevorwamy@gmail.com>
2022-07-06 15:05:47 +0300

    librarian table

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Librarian(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Password = models.CharField(max_length=50)
+    UserName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.UserName
