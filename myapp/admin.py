4e561860890a6f6f8e5cb9653e27571c0a772f80
Ronald Wamimbi <trevorwamy@gmail.com>
2022-07-06 14:57:30 +0000

    update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
-from .models import Librarian
+from .models import Librarian, Book, Student, Borrowers
 
 admin.site.register(Librarian)
-from .models import Book
-
-# Register your models here.
 admin.site.register(Book)
-from .models import Student
-
-# Register your models here.
 admin.site.register(Student)
-from .models import Borrowers
-
 admin.site.register(Borrowers)
 
