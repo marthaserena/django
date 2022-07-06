110f34e8ff80b6af90c284eb7439a03bad2d6542
marthaserena <mearthaserena@gmail.com>
2022-07-06 13:25:25 +0300

    Models file 1

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    UserName= models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    FirstName= models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    Email =models.CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.UserName
