b558a8c67bfec90ba1f8a6091873e4e6f2c6b38a
Amanya Allan Kayumbu <allan@Amanyas-MacBook-Pro.local>
2022-07-06 13:31:53 +0300

    Changes made to the models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    author = models.CharField(max_length=100)
+    book_title = models.CharField(max_length=200)
+    publish_date = models.DateField()
+    book_id = models.CharField(max_length=100)
+    book_image = models.CharField(max_length=1000)
+    book_collection = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.book_title
