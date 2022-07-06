f32c3667aaa5cfabbb08db5516ac2dfba7dd4a01
AISHA SSERUBIDDE <aishasser6@gmail.com>
2022-07-06 10:00:16 +0000

    Update myapp/models.py, myapp/admin.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Borrowers (models.Model):
+    book_id= models.CharField
+    Return_date=models.DateField
+    Date_taken=models.DateField
