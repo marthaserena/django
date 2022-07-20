3d5e6d32cc0ad665320c91a10d1b4e6f42ce1525
allanalmax13 <allanalmax13@gmail.com>
2022-07-20 20:30:53 +0300

    static files

--- project1/settings.py
+++ project1/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
