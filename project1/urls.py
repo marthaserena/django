852dd5e33380de36014e65fa594577851d34f798
allanalmax13 <allanalmax13@gmail.com>
2022-08-09 23:51:10 +0300

    working on css deployment

--- project1/urls.py
+++ project1/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('myapp.urls'))
 
     url (r'^media/(?P<path.*)$' , serve,{' document_root':       settings.MEDIA_ROOT}),
     url (r'^static/(?P<path.*)$', serve,{'document_root': settings.STATIC_ROOT}),
